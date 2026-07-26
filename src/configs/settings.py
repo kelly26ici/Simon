6c5a9e8ba3004f556ca22567dd74feb0aaac616b
kelly26ici <rexk638@gmail.com>
2026-07-26 14:56:22 +0300

    hhsjs

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -16,7 +16,7 @@ load_dotenv()
 #======================================================
 
 
-GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 
 GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
 
